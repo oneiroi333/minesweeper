1362a5c1d59ced75457ee3a95ed75f943cdf4086
Pircher Simon <pircher.simon@hotmail.de>
2019-05-17 18:51:53 +0200

    test matrix

--- test/test_matrix.c
+++ test/test_matrix.c
@@ -1,49 +1,72 @@
 #include <stdio.h>
 #include "../src/core/matrix.h"
 
-void
-matrix_dump(struct matrix *m)
-{
-	int i, j;
-
-	for (i = 0; i < m->rows; ++i) {
-		for (j = 0; j < m->columns; ++j) {
-			printf("%i ", matrix_get(m, i, j));
-		}
-		printf("\n");
-	}
-}
-
 int
 main(void)
 {
-	struct matrix *m1, *m2;
+	struct matrix *m;
+	int i, j, val, err, error;
 
-	printf("Create matrix m1 (5, 5, 1)\n");
-	m1 = matrix_init(5, 5, 1);
-	if (m1) {
-		matrix_dump(m1);
-		printf("Set field (2, 2) -> 3\n");
-		matrix_set(m1, 2, 2, 3);
-		matrix_dump(m1);
+	error = 0;
+	printf("Testing matrix... ");
+
+	/* matrix_init */
+	m = matrix_init(5, 5, 1);
+	if (m) {
+		if (m->rows != 5 || m->columns != 5) {
+			error = 1;
+		}
+		for (i = 0; i < 5; ++i) {
+			for (j = 0; j < 5; ++j) {
+				if (m->table[i][j] != 1) {
+					error = 1;
+				}
+			}
+		}
 	} else {
-		printf("Error: Couldnt create matrix m1\n");
+		error = 1;
+	}
+	if (error) {
+		printf("matrix_init failed\n");
+		goto exit_error;
 	}
 
-	printf("Create matrix m2 (15, 15, 2)\n");
-	m2 = matrix_init(15, 15, 2);
-	if (m2) {
-		matrix_dump(m2);
-		printf("Set field (10, 10) -> 9\n");
-		matrix_set(m2, 10, 10, 9);
-		matrix_dump(m2);
+	/* matrix_reinit */
+	err = matrix_reinit(m, 4, 4, 0);
+	if (err == -1) {
+		error = 1;
 	} else {
-		printf("Error: Couldnt create matrix m2\n");
+		for (i = 0; i < 4; ++i) {
+			for (j = 0; j < 4; ++j) {
+				if (m->table[i][j] != 0) {
+					error = 1;
+				}
+			}
+		}
+	}
+	if (error) {
+		printf("matrix_reinit failed\n");
+		goto exit_error;
 	}
 
-	matrix_free(m1);
-	matrix_free(m2);
+	/* matrix_set */
+	matrix_set(m, 2, 2, 3);
+	if (m->table[2][2] != 3) {
+			printf("matrix_set failed\n");
+			goto exit_error;
+	}
 
+	/* matrix_get */
+	val = matrix_get(m, 2, 2);
+	if (val != 3) {
+			printf("matrix_get failed\n");
+			goto exit_error;
+	}
+
+	matrix_free(m);
+	
+	printf("OK\n");
+exit_error:
 	return 0;
 }
 
